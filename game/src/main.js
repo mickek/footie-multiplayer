2c6958de0ee6b587d4720fb6acd01b1953d26c2e
Michał Kłujszo <michal.klujszo@10clouds.com>
2011-04-16 12:27:27 +0200

    cocos2d something working

---
+++ game/src/main.js
@@ -0,0 +1,5 @@
+/*globals module exports resource require window*/
+/*jslint undef: true, strict: true, white: true, newcap: true, browser: true, indent: 4 */
+"use strict";
+
+require('./cocos2d/TileMapTest')
