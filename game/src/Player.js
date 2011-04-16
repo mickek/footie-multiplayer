f81bc1bb63eebb4c2551f283b6189754fe015c0b
Aleksandra Klapcinska <aleksandra.klapcinska@gmail.com>
2011-04-16 14:28:00 +0200

    sprites

--- game/src/Player.js
+++ game/src/Player.js
@@ -7,13 +7,13 @@ var Player = cocos.nodes.Node.extend({
 
         var sprite = cocos.nodes.Sprite.create({
                          file: '/resources/sprites.png',
-                         rect: new geom.Rect(0, 0, 64, 16)
+                         rect: new geom.Rect(64, 0, 16, 16)
                      });
 
         sprite.set('anchorPoint', new geom.Point(0, 0));
         this.addChild({child: sprite});
         this.set('contentSize', sprite.get('contentSize'));
-    }
+    
 });
 
 module.exports = Player;
