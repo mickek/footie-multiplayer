a4b5941a9e72849ecfbfd5efc31afb32e6264242
Aleksandra Klapcinska <aleksandra.klapcinska@gmail.com>
2011-04-16 15:08:56 +0200

    moving players

--- game/src/Player.js
+++ game/src/Player.js
@@ -1,5 +1,6 @@
 var cocos = require('cocos2d'),
-    geom = require('geometry');
+    geom = require('geometry'), 
+    util = require('util');
 
 var Player = cocos.nodes.Node.extend({
 
@@ -16,12 +17,26 @@ var Player = cocos.nodes.Node.extend({
         sprite.set('anchorPoint', new geom.Point(0, 0));
         this.addChild({child: sprite});
         this.set('contentSize', sprite.get('contentSize'));
+        this.set('velocity', new geom.Point(0,0));
+        this.scheduleUpdate();
     }, 
     
     setVelocity: function(vel){
         this.set('velocity', vel);
-    }   
+        var rad = Math.atan(vel.y/vel.x);
+        rad = rad * (360 / Math.PI)
+        this.set('rotation', rad);
+    },
 
+    update: function(dt){
+            var pos = util.copy(this.get('position')),
+                vel = util.copy(this.get('velocity'));
+
+            pos.x += dt * vel.x;
+            pos.y += dt * vel.y;
+            this.set('position', pos)
+            }
+    
 });
 
 module.exports = Player;
